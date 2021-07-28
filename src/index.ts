761167a7f55715b001ecb4e27404db56011ca182
mkosir <kosir.mar@gmail.com>
2021-07-28 22:35:14 +0200

    fix: refactor rule

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
-import { Rule } from 'eslint';
+// import { Rule } from 'eslint';
 
 import { disallowArrayFunction } from './rules/disallowArrayFunction';
 
-const plugin: { rules: { [key: string]: Rule.RuleModule }; [key: string]: any } = {
+// const plugin: { rules: { [key: string]: Rule.RuleModule }; [key: string]: any } = {
+//   rules: {
+//     'no-reduce': {
+//       create: (context) => disallowArrayFunction(context, 'reduce'),
+//     },
+//     'no-reduce-right': {
+//       create: (context) => disallowArrayFunction(context, 'reduceRight'),
+//     },
+//   },
+
+//   configs: {
+//     recommended: {
+//       plugins: ['no-array-reduce'],
+//       rules: {
+//         'no-array-reduce/no-reduce': 'error',
+//         'no-array-reduce/no-reduce-right': 'error',
+//       },
+//     },
+//   },
+// };
+
+module.exports = {
   rules: {
     'no-reduce': {
       create: (context) => disallowArrayFunction(context, 'reduce'),
@@ -22,5 +43,3 @@ const plugin: { rules: { [key: string]: Rule.RuleModule }; [key: string]: any } 
     },
   },
 };
-
-module.exports = plugin;
