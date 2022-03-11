ad0d39bf93657a178f79278808edc2d384bd0350
mkosir <kosir.mar@gmail.com>
2022-03-11 09:32:45 +0100

    refactor: types

--- src/rules/disallowArrayFunction.ts
+++ src/rules/disallowArrayFunction.ts
@@ -1,6 +1,9 @@
 import { Rule } from 'eslint';
 
-export const disallowArrayFunction = (context: Rule.RuleContext, disallowFunctionName: string): Rule.RuleListener => {
+export const disallowArrayFunction = (
+  context: Rule.RuleContext,
+  disallowFunctionName: 'reduce' | 'reduceRight',
+): Rule.RuleListener => {
   return {
     MemberExpression(node) {
       // @ts-ignore
